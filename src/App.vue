edd91901050592f6f5e9962c3406ae79c34ff09e
owentr1369 <tamvaa1306@gmail.com>
2022-05-28 14:54:43 +0700

    fontawesome config

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view />
+    <SideBar />
   </div>
 </template>
+<script>
+import SideBar from "./components/SideBar.vue";
+export default {
+  name: "App",
+  components: {
+    SideBar,
+  },
+};
+</script>
 
 <style lang="scss">
 * {
--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* add icons to the library */
+library.add(fas);
+
+/* add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
